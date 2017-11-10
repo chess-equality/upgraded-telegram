39cd5409917d1c6a2ef849451a5a6cbd215454f7
chess-equality <chess-equality>
2017-11-10 23:04:50 +0800

    - Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ERC20 Token Smart Contract Testing
+
+### Prerequisites
+1. JDK 1.8+
+2. Includes Maven wrapper (mvnw)
+
+### Run
+1. ./mvnw clean test
+2. Profiles coming soon
---
+++ src/main/java/net/kreatious/ethereum/upgradedtelegram/UpgradedtelegramApplication.java
@@ -0,0 +1,12 @@
+package net.kreatious.ethereum.upgradedtelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpgradedtelegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpgradedtelegramApplication.class, args);
+	}
+}
