b132bb7331402d453bcc85bf7e3db3832502cd2e
chess-equality <chessequality@gmail.com>
2017-11-12 23:02:51 +0800

    - Added private and testnet profiles (defaults to private) - Added initial test for blockchain connectivity

--- README.md
+++ README.md
@@ -5,5 +5,5 @@
 2. Includes Maven wrapper (`mvnw`)
 
 ### Run
-1. `./mvnw clean test`
-2. Profiles coming soon
+1. `./mvnw [-D"spring.profiles.active"=private|testnet] clean test`
+2. If profile is not given, defaults to `private`.
--- src/test/java/net/kreatious/ethereum/upgradedtelegram/UpgradedtelegramApplicationTests.java
+++ src/test/java/net/kreatious/ethereum/upgradedtelegram/UpgradedtelegramApplicationTests.java
@@ -2,15 +2,30 @@ package net.kreatious.ethereum.upgradedtelegram;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.web3j.protocol.admin.Admin;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UpgradedtelegramApplicationTests {
-
+	
+	private final Logger log = LoggerFactory.getLogger(UpgradedtelegramApplicationTests.class);
+	
+	@Value("${endpoint}")
+	private String endpoint;
+	
+	@Autowired
+	private Admin admin;
+	
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		
+		// Test for blockchain connectivity
+		assert(admin.netListening().sendAsync().get().isListening());
 	}
-
 }
